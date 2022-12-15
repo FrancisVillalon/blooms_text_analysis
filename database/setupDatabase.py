d0c57b4cd3c66ec78ae148e701e5d540ffdd5379
francis <francisvil3213@gmail.com>
2022-12-15 21:10:06 +0800

    Minor changes

--- database/setupDatabase.py
+++ database/setupDatabase.py
@@ -1,3 +1,108 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:246beee03b224e402e1b6e207efbf06a1daa8227e59d2a5ba628adaa37db0363
-size 3451
+import os
+
+import psycopg
+import tomllib
+from tqdm import tqdm
+import pandas as pd
+
+# Initial setup of database tables and other entries
+
+word_path = "./database/other_data/words"
+
+# database config
+with open("./config/db-config.toml", "rb") as f:
+    data = tomllib.load(f)
+    db_user = data["database"]["db_user"]
+    db_secret = data["database"]["db_pass"]
+    db_name = data["database"]["db_name"]
+
+mapLevel = {
+    "Fresh/entry level": 0,
+    "Non-executive": 1,
+    "Junior Executive": 2,
+    "Manager": 2,
+    "Executive": 3,
+    "Professional": 3,
+    "Middle Management": 3,
+    "Senior Management": 4,
+    "Senior Executive": 4,
+}
+
+# create connection and perform setup
+with psycopg.connect(dbname=db_name, user=db_user, password=db_secret) as conn:
+    with conn.cursor() as cur:
+        # Setup blooms taxonomy terms
+        print("=> Setting up blooms taxonomy terms")
+        for fn in tqdm(os.listdir(word_path)):
+            word_category = fn.split(".")[0]
+            cur.execute(
+                f"""
+                CREATE TABLE IF NOT EXISTS {word_category}_keywords (
+                    keywords text unique
+                ) 
+                """
+            )
+            with open(os.path.join(word_path, fn), "r") as f:
+                for word in f:
+                    try:
+                        query = f"INSERT INTO {word_category}_keywords (keywords) VALUES (%s)"
+
+                        params = (word.strip(),)
+                        cur.execute(query, params)
+                    except Exception as e:
+                        conn.rollback()
+                        continue
+                    conn.commit()
+        # Setup level to job position mapping
+        cur.execute(
+            f"""
+            CREATE TABLE IF NOT EXISTS job_levels (
+                job_title text unique,
+                job_level int
+
+            )
+            """
+        )
+        print("=> Setting up position level table")
+        for level in tqdm(mapLevel.keys()):
+            try:
+                cur.execute(
+                    f"INSERT INTO job_levels VALUES (%s,%s)", (level, mapLevel[level])
+                )
+            except Exception as e:
+                conn.rollback()
+                continue
+            conn.commit()
+
+        # Setup job text table
+        print("=> Setting up job text table")
+        cur.execute(
+            f"""
+            CREATE TABLE IF NOT EXISTS job_postings (
+                job_link text unique,
+                job_text text,
+                last_posted_date date,
+                address text,
+                company text,
+                num_of_applications int,
+                lower_salary_range int,
+                upper_salary_range int,
+                industry text,
+                position_level text,
+                min_exp_years int 
+            )
+            """
+        )
+        DATA = pd.read_csv("./database/other_data/TEXTDATA.csv", index_col=0)
+        DATA = DATA.reset_index()
+        for i, k in tqdm(DATA.iterrows(), total=DATA.shape[0]):
+            try:
+                query = f"INSERT INTO job_postings VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                params = tuple(k.values)
+                cur.execute(query, params)
+                print(f"INSERTED INTO job_postings: {params[0]}")
+            except Exception as e:
+                conn.rollback()
+                continue
+            # commit transactions
+            conn.commit()
--- run_ws.py
+++ run_ws.py
@@ -1,3 +1,49 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:90bb8ba10ff9d539a8e066d8ee27a95b15f7db018095a6813616bbc58ec3a9d3
-size 1312
+from webscraper.ws import *
+from multiprocessing import Pool
+import pandas as pd
+import traceback
+from database.db_conn import create_conn
+import pandas.io.sql as sqlio
+import sys
+import os
+
+
+def init_webscrape(n, positionLevel):
+    print(f"STARTING JOB FOR {positionLevel}")
+    try:
+        scraper = BloomsWebScraper(positionLevel)
+    except Exception as e:
+        print(e)
+    finally:
+        scraper.kill_driver()
+        return
+
+
+def init_textscrape(link_list):
+    print(f"STARTING TEXT SCRAPE FOR {len(link_list)} PAGES.")
+    try:
+        scraper = BloomsTextScraper()
+        scraper.get_n_page_text(link_list)
+    except Exception as e:
+        print(e)
+    finally:
+        scraper.kill_driver()
+        return
+
+
+def get_and_consolidate_nlinks(
+    n, job_levels
+) -> "Iterate over job_levels list and grab links from n pages":
+    pool_inputs = [(n, e) for e in job_levels]
+    # To get links from pages
+    with Pool(processes=5) as pool:
+        mframe = pd.DataFrame(None)
+        pool.starmap(init_webscrape, pool_inputs)
+    return
+
+
+def get_and_consolidate_text(link_list, n) -> "Process list of links in n sized chunks":
+    pool_inputs = [link_list[i : i + n] for i in range(0, len(link_list), n)]
+    with Pool(processes=5) as pool:
+        pool.map(init_textscrape, pool_inputs)
+    return
